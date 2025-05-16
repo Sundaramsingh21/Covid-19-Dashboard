afa6aa2353ab287dd3102e27c565312555575a1c
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2025-05-16 19:32:52 +0530

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1 className="title">COVID-19 and Population Dashboard</h1>
+      <h1 className="title">COVID-19 Dashboard</h1>
       
     </div>
   )
