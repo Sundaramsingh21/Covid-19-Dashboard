88a9aab8739e9c9621cb5fa897968b52ecabe19f
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2025-05-16 14:49:21 +0530

    Update PieChart.jsx

--- Frontend/src/components/PieChart.jsx
+++ Frontend/src/components/PieChart.jsx
@@ -6,14 +6,14 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart = ({ stats }) => {
   return (
-    <div className="chart-container">
+   <div className="chart-container">
       <h3>Pie Chart</h3>
       <Pie data={{
-        labels: ['Total Population','Cases', 'Recovered', 'Deaths'],
+        labels: ['Cases', 'Recovered', 'Deaths'],
         datasets: [{
           label: 'People',
-          data: [stats.population, stats.cases, stats.recovered, stats.deaths],
-          backgroundColor: ['#e4e49a','blue', 'green', 'red'],
+          data: [stats.cases, stats.recovered, stats.deaths],
+          backgroundColor: ['#3498db', '#2ecc71', '#e74c3c'],
         }]
       }} />
     </div>
